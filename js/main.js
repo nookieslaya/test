23f5f2efb2dd2a319fe690f2880f3381a2933b0f
nookieslaya <56317968+nookieslaya@users.noreply.github.com>
2020-10-21 14:02:18 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// skrypt naprawiający nawigację (zamyka się po kliknięciu w cokolwiek na mobilce)
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+        $(".navbar-toggler").click();
+    }
+});
+
+
+// slickJS
